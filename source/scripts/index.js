33cb19f995b2d0ae5ad55fe9d02d53d8025ffce4
MurtazinRenat <122800226+MurtazinRenat@users.noreply.github.com>
2024-06-05 11:24:14 +0300

    Добавляет декоративные элементы (#11)
    * Добавляет декоративные элементы
    
    * Добавляет исправления по замечаниям в размерах разметки
    
    * Добавляет исправление сетки некоторых блоков и PP
    
    * Добавляет метки для автоматической проверки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,34 @@
 /* в этот файл добавляет скрипты*/
+//menu
+const navigation = document.querySelector('.navigation');
+const navList = document.querySelector('.site-list');
+const navToggle = document.querySelector('.navigation__toggle');
+const navClue = document.querySelector('.navigation__open');
+
+navList.classList.remove('site-list--nojs');
+navToggle.classList.remove('navigation__toggle--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navList.classList.contains('site-list--closed')) {
+    navList.classList.remove('site-list--closed');
+    navToggle.classList.add('navigation__toggle--closed');
+    navClue.textContent = 'Закрыть меню.';
+  } else {
+    navList.classList.add('site-list--closed');
+    navToggle.classList.remove('navigation__toggle--closed');
+    navClue.textContent = 'Открыть меню.';
+  }
+});
+
+navList.style.top = `${navigation.offsetHeight }px`;
+
+window.addEventListener('resize', (e) => {
+  navList.style.top = `${navigation.offsetHeight }px`;
+});
+
+//map
+const wrapper = document.querySelector('.invite__map');
+const image = document.querySelector('.cinvite__map-image');
+
+wrapper.classList.remove('invite__map--nojs');
+image.classList.add('invite__map-image--nojs');
